7f8a85a33acaad1879f8c79591ebfaa4ae4932d4
Nikita <ncheusov@inbox.ru>
2022-09-12 23:54:12 +0300

    make getting response from api

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './components/app/App';
+import MarvelService from './services/MarvelService';
 
 import './style/style.scss';
+
+const marvelService = new MarvelService();
+
+marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <StrictMode>
         <App />
+
     </StrictMode>
 );
 
