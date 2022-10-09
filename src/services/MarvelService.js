69fef1bc1d49f2b8fe1ae772283f12f61ffe29e0
Nikita <ncheusov@inbox.ru>
2022-10-09 19:24:47 +0300

    update

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -10,15 +10,39 @@ class MarvelService {
         }
 
         return await res.json();
-    }
+    };
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
-    }
+    getAllCharacters = async () => {
+        const res = await this.getResource(
+            `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+        );
+        return res.data.results.map(this._transformCharacter);
+    };
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-    }
+    getCharacter = async (id) => {
+        const res = await this.getResource(
+            `${this._apiBase}characters/${id}?${this._apiKey}`
+        );
+        return this._transformCharacter(res.data.results[0]);
+    };
+
+    _transformCharacter = (char) => {
+        if (!char.description) {
+            char.description = "Описания персонажа пока что нет";
+          }
+       
+          if (char.description.length > 220) {
+            char.description = char.description.slice(0, 215) + "…";
+          }
+      
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+        }
+    };
 }
 
 export default MarvelService;
