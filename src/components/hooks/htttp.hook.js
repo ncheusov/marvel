bc2dfdb9d65a8a48e553560bf042a7e476e2174f
Nikita <ncheusov@inbox.ru>
2022-10-26 18:56:46 +0300

    start building custom hook

---
+++ src/components/hooks/htttp.hook.js
@@ -0,0 +1,16 @@
+import { useState, useCallback } from "react";
+
+export const useHttp = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const request = useCallback(async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json'}) => {
+        setLoading(true);
+
+        try {
+            const response = await fetch(url, {method, body, headers});
+        } catch(e) {
+            
+        }
+    }, []) 
+}
