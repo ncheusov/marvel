c601bd8102ae98f355a77eab8ba1b43fb5ecb330
Nikita <ncheusov@inbox.ru>
2022-10-30 22:03:28 +0300

    some change

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -22,7 +22,7 @@ const App = () => {
                 <main>
                     <Suspense fallback={<Spinner />}>
                         <Routes>
-                            <Route path="" element={<MainPage />} />
+                            <Route path="/" element={<MainPage />} />
                             <Route path="comics" element={<ComicsPage />} />
                             <Route path="comics/:comicId" element={<SingleComicPage />} />
                             <Route path="*" element={<Page404 />} />
